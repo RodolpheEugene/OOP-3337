41fb8b601a028f6f20e8b396308dae7571010b21
RodolpheEugene <92120435+RodolpheEugene@users.noreply.github.com>
2022-10-21 12:39:11 -0400

    Update Lab2PartA.cpp

--- OOP Labs HW/Exam 1 Study Guide/Lab2PartA.cpp
+++ OOP Labs HW/Exam 1 Study Guide/Lab2PartA.cpp
@@ -1,25 +1,25 @@
-//#include <iostream>
-//
-//using namespace std;
-//int getDigit(int n)
-//{
-//	// to count characters in given integer
-//	if (n == 0)
-//		return 1;
-//	int count = 0;
-//	while (n != 0)
-//	{
-//		n = n / 10;
-//		++count;
-//	}
-//	return count;
-//}
-//int main()
-//{
-//	// get user input
-//	long long getInput;
-//	cout << "Enter an integer: ";
-//	cin >> getInput;
-//	cout << "The number of digits in " << getInput << " is " << getDigit(getInput);
-//
-//}
+#include <iostream>
+
+using namespace std;
+int getdigit(int n)
+{
+	// to count characters in given integer
+	if (n == 0)
+		return 1;
+	int count = 0;
+	while (n != 0)
+	{
+		n = n / 10;
+		++count;
+	}
+	return count;
+}
+int main()
+{
+	// get user input
+	long long getinput;
+	cout << "enter an integer: ";
+	cin >> getinput;
+	cout << "the number of digits in " << getinput << " is " << getdigit(getinput);
+
+}
