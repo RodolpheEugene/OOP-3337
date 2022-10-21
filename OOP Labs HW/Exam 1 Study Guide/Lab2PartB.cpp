a711c596a0ce9b438d2d365112b3c96f81d64ed7
RodolpheEugene <92120435+RodolpheEugene@users.noreply.github.com>
2022-10-21 12:40:49 -0400

    Update Lab2PartB.cpp

--- OOP Labs HW/Exam 1 Study Guide/Lab2PartB.cpp
+++ OOP Labs HW/Exam 1 Study Guide/Lab2PartB.cpp
@@ -1,16 +1,16 @@
-//#include <iostream>
-//#include <iomanip>
-//
-//using namespace std;
-//int main()
-//{
-//
-//	// set up table with meters and feet column
-//	cout << setw(5) << "meters" << setw(8) << "feet" << endl;
-//	for (int m = 1; m < 16; m++)
-//	{
-//		// print the values with 3 decimals and use setw to align uder feet column
-//		cout << left << setw(10) << m <<  setprecision(3) << fixed << m * 3.280 << endl;
-//
-//	}
-//}
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+
+	// set up table with meters and feet column
+	cout << setw(5) << "meters" << setw(8) << "feet" << endl;
+	for (int m = 1; m < 16; m++)
+	{
+		// print the values with 3 decimals and use setw to align uder feet column
+		cout << left << setw(10) << m <<  setprecision(3) << fixed << m * 3.280 << endl;
+
+	}
+}
